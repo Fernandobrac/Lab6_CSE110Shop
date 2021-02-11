a7aea321b1d7a099146cd55eec3c0122d240bbcc
Fernando Bracamonte <56770413+Fernandobrac@users.noreply.github.com>
2021-02-11 00:00:01 -0800

    Added simple fetch code

--- scripts/script.js
+++ scripts/script.js
@@ -2,4 +2,12 @@
 
 window.addEventListener('DOMContentLoaded', () => {
   // TODO
+  fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(data => {
+      localStorage.setItem("product", JSON.stringify(data));
+    })
+
+    //myStorage = window.localStorage;
+    
 });
