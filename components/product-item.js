c973fc12044d9d652c9b325a66bd8aa1806c47f9
Fernando Bracamonte <fernandobrac@hotmail.com>
2021-02-13 23:46:04 -0800

    finished lab

--- components/product-item.js
+++ components/product-item.js
@@ -2,6 +2,148 @@
 
 class ProductItem extends HTMLElement {
   // TODO
+  //classes and constructor: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+  constructor(id, price, title, image){
+		
+		super();
+
+    //local storage 
+		myStorage = window.localStorage;
+
+    //Taken from: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements 
+    // https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM 
+		//we attach a shadow DOM to the element
+    let shadow = this.attachShadow({mode: 'open'});
+		
+    let cartCount = document.getElementById('cart-count');
+		let itemsInCart = JSON.parse(myStorage.getItem('itemsInCart'));
+		let cartButton = document.createElement('cartButton');
+
+		////Taken from: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_custom_elements 
+		let li = document.createElement('li');
+		li.setAttribute('class', 'product');
+		
+		let itemPrice = document.createElement('p');
+		itemPrice.setAttribute('class', 'price');
+		itemPrice.textContent = '$' + price;
+		
+		let itemTitle = document.createElement('p');
+		itemTitle.setAttribute('class', 'title');
+		itemTitle.textContent = title;
+		
+		let itemImage = document.createElement('img');
+		itemImage.src = image;
+		itemImage.alt = title;
+		itemImage.width = 200;
+
+    //Modify button text: if item is not in cart array, then:
+		if(!itemsInCart.includes(id)){
+			cartButton.textContent = 'Add to Cart';
+		}
+		else{
+			cartButton.textContent = 'Remove from Cart';
+		}
+		
+		//add on click functionality to button:
+		cartButton.onclick = function () {
+			if (cartButton.textContent == 'Remove from Cart'){
+
+				itemsInCart = JSON.parse(myStorage.getItem('itemsInCart'));
+				cartButton.textContent = "Add to Cart";
+				cartCount.textContent = parseInt(cartCount.textContent) - 1;
+				itemsInCart.splice(itemsInCart.indexOf(id), 1);
+				myStorage.setItem('itemsInCart', JSON.stringify(itemsInCart));
+				alert('Item was removed from your cart');
+			}
+			else{
+				itemsInCart = JSON.parse(myStorage.getItem('itemsInCart'));
+				cartButton.textContent = "Remove from Cart";
+				cartCount.textContent = parseInt(cartCount.textContent) + 1;
+				itemsInCart.push(id);
+				myStorage.setItem("itemsInCart", JSON.stringify(itemsInCart));
+				alert('Item was added to your cart');
+			}
+		}
+		
+		//Styling the shadow DOM:
+    //Taken from https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM 
+		let style = document.createElement('style');
+		//CSS format copied from styles.css file:
+    style.textContent = `
+    .price {
+      color: green;
+      font-size: 1.8em;
+      font-weight: bold;
+      margin: 0;
+    }
+    .product {
+      align-items: center;
+      background-color: white;
+      border-radius: 5px;
+      display: grid;
+      grid-template-areas: 
+      'image'
+      'title'
+      'price'
+      'add';
+      grid-template-rows: 67% 11% 11% 11%;
+      height: 450px;
+      filter: drop-shadow(0px 0px 6px rgb(0,0,0,0.2));
+      margin: 0 30px 30px 0;
+      padding: 10px 20px;
+      width: 200px;
+    }
+    .product > cartButton {
+      background-color: rgb(255, 208, 0);
+      border: none;
+      border-radius: 5px;
+      color: black;
+      justify-self: center;
+      max-height: 35px;
+      padding: 8px 20px;
+      transition: 0.1s ease all;
+    }
+    .product > cartButton:hover {
+      background-color: rgb(255, 166, 0);
+      cursor: pointer;
+      transition: 0.1s ease all;
+    }
+
+    /* Modified to max-height and max-width to that the 
+      images do not exceed the size of the "container" 
+    */
+
+    .product > img {
+      align-self: center;
+      justify-self: center;
+      max-width: 100%;  /*added max-width*/
+      max-height: 100%  /*added max-height*/
+    }
+    .title {
+      font-size: 1.1em;
+      margin: 0;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+    }
+    .title:hover {
+      font-size: 1.1em;
+      margin: 0;
+      white-space: wrap;
+      overflow: auto;
+      text-overflow: unset;
+    }`;
+		
+    // attach the created elements to the shadow dom
+    //Taken from: https://developer.mozilla.org/en-US/docs/Web/Web_Components/Using_shadow_DOM 
+		shadow.appendChild(style, li);
+		shadow.appendChild(li);
+		li.appendChild(itemImage);
+		li.appendChild(itemTitle);
+		li.appendChild(itemPrice);
+		li.appendChild(cartButton);
+		
+	}
 }
 
 customElements.define('product-item', ProductItem);
--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,45 @@
 // Script.js
 
-window.addEventListener('DOMContentLoaded', () => {
-  // TODO
-  fetch('https://fakestoreapi.com/products')
-    .then(response => response.json())
-    .then(data => {
-      localStorage.setItem("product", JSON.stringify(data));
-    })
+const website = 'https://fakestoreapi.com/products';
+let cartCount = document.getElementById('cart-count');
+let productList = document.getElementById('product-list');
+//Local storage: https://developer.mozilla.org/en-US/docs/Web/API/Window/localStorage
+myStorage = window.localStorage;
 
-    //myStorage = window.localStorage;
-    
+//Add elements added to cart to storage to keep track of
+if (myStorage.getItem('itemsInCart') == null){
+  myStorage.setItem('itemsInCart', JSON.stringify(new Array()))
+}
+let itemsInCart = JSON.parse(myStorage.getItem('itemsInCart'));
+cartCount.textContent = itemsInCart.length;
+
+window.addEventListener('DOMContentLoaded', () => {
+	let prodArr = [];
+
+  //To fetch: https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch 
+	if (myStorage.getItem('products') == null) {
+		fetch(website)
+			.then(response => response.json())
+			.then(data => {
+				myStorage.setItem('products', JSON.stringify(data));
+				prodArr = data;
+				loadContent(prodArr);
+			});
+	}
+	else{
+		prodArr = JSON.parse(myStorage.getItem('products'))
+		loadContent(prodArr);
+	}
 });
+
+//
+function loadContent(prodArr) {
+	let i = 0;
+	for (; i < prodArr.length; i++){
+    //create a ProductItem object and pass in variables to constructor
+		let newProduct = new ProductItem(prodArr[i]['id'], prodArr[i]['price'],  
+			prodArr[i]['title'], prodArr[i]['image']);
+    //add this element to the produxt list 
+		productList.appendChild(newProduct);
+	}
+}
